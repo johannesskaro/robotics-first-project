2be2e9e1c2dc52398fdccb758f57f51569da721f
johannrs <johannrs@stud.ntnu.no>
2024-04-17 11:48:28 +0200

    Changed from NED to ENU and set params in launch file

--- src/gps_to_odom.cpp
+++ src/gps_to_odom.cpp
@@ -13,48 +13,49 @@ private:
   ros::Publisher gps_odom_pub = n.advertise<nav_msgs::Odometry>("gps_odom", 1);
   ros::Subscriber gps_sub;
 
-  double lat_zero;
-  double lon_zero;
-  double alt_zero;
+  double lat_ref;
+  double lon_ref;
+  double alt_ref;
 
   double a = 6378137; // Semi-major axis of earth given in meters
-  double b = 6356752.3142; // Semi-minor axis of earth given in meters
-  double e = 0.0818191908426; // Eccentricity of earth
+  double b = 6356752; // Semi-minor axis of earth given in meters
+  double e_squared = 1 - pow(b, 2) / pow(a, 2);  //  0.0818191908426; // Eccentricity of earth
 
   double N_prev = 0;
   double E_prev = 0;
-  double D_prev = 0;
+  double U_prev = 0;
 
 public:
   gps_to_odom() {
     gps_sub = n.subscribe("/fix", 1, &gps_to_odom::gps_callback, this);
+    n.getParam("lat_r", lat_ref);
+    n.getParam("lon_r", lon_ref);
+    n.getParam("alt_r", alt_ref);
   }
 
   void gps_callback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
-    ROS_INFO("Recieved msg :D");
-
     double lat = msg->latitude;
     double lon = msg->longitude;
     double alt = msg->altitude;
 
     // convert from latitude-longitude-altitude to Cartesian ECEF
-    double R = a / sqrt(1 - pow(e, 2) * pow(sin(lat), 2));
+    double R = a / sqrt(1 - e_squared * pow(sin(lat), 2));
     double X = (R + alt) * cos(lat) * cos(lon);
     double Y = (R + alt) * cos(lat) * sin(lon);
     double Z = ((pow(a, 2) / pow(b, 2)) * R + alt) * sin(lat);
   
-    double X_zero = (R + alt_zero) * cos(lat_zero) * cos(lon_zero);
-    double Y_zero = (R + alt_zero) * cos(lat_zero) * sin(lon_zero);
-    double Z_zero = ((pow(a, 2) / pow(b, 2)) * R + alt_zero) * sin(lat_zero);
+    double X_zero = (R + alt_ref) * cos(lat_ref) * cos(lon_ref);
+    double Y_zero = (R + alt_ref) * cos(lat_ref) * sin(lon_ref);
+    double Z_zero = ((pow(a, 2) / pow(b, 2)) * R + alt_ref) * sin(lat_ref);
 
     double X_diff = X - X_zero;
     double Y_diff = Y - Y_zero;
     double Z_diff = Z - Z_zero;
     
-    // convert from Cartesian ECEF to NED
-    double N = (- sin(lat_zero) * cos(lon_zero)) * X_diff + (- sin(lat_zero) * sin(lon_zero)) * Y_diff + (cos(lat_zero)) * Z_diff;
-    double E = (- sin(lon_zero)) * X_diff + (cos(lon_zero)) * Y_diff + (0) * Z_diff;
-    double D = (- cos(lat_zero) * cos(lon_zero)) * X_diff + (- cos(lat_zero) * sin(lon_zero)) * Y_diff + (- sin(lat_zero)) * Z_diff;
+    // convert from Cartesian ECEF to ENU
+    double E = (- sin(lon_ref)) * X_diff + (cos(lon_ref)) * Y_diff + (0) * Z_diff;
+    double N = (- sin(lat_ref) * cos(lon_ref)) * X_diff + (- sin(lat_ref) * sin(lon_ref)) * Y_diff + (cos(lat_ref)) * Z_diff;
+    double U = (cos(lat_ref) * cos(lon_ref)) * X_diff + (cos(lat_ref) * sin(lon_ref)) * Y_diff + (sin(lat_ref)) * Z_diff;
 
     // construct the odom message
     nav_msgs::Odometry odom_msg;
@@ -62,24 +63,26 @@ public:
     geometry_msgs::Quaternion quat_msg;
     tf2::Quaternion quat_tf;
 
-    point.x = N;
-    point.y = E;
-    point.z = D;
+    point.x = E;
+    point.y = N;
+    point.z = U;
     odom_msg.pose.pose.position = point;
 
     // estimate heading from consecutive poses
-    double heading = atan2(N - N_prev, - (E - E_prev));
+    double heading = atan2(N - N_prev, E - E_prev);
     quat_tf.setRPY(0, 0, heading);
     quat_msg = tf2::toMsg(quat_tf);
     odom_msg.pose.pose.orientation = quat_msg;
 
     // publish
+    // odom_msg.child_frame_id = "reach_rs";
+    odom_msg.header.frame_id = "reach_rs";
     gps_odom_pub.publish(odom_msg);
 
     // update
-    N_prev = N;
     E_prev = E;
-    D_prev = D;
+    N_prev = N;
+    U_prev = U;
   }
 
 };
@@ -89,14 +92,10 @@ int main(int argc, char **argv){
 
     ros::init(argc, argv, "gps_to_odom");
     gps_to_odom gps_to_odom;
-    //ros::Rate loop_rate(10);
-
     ROS_INFO("%s", "hello gps_to_odom!");
 
   	while (ros::ok()){
-    	
     	ros::spinOnce();
-      //loop_rate.sleep();
     }
   	return 0;
 }
